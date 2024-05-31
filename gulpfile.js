6a3b95e101316985257e98dbc805a219933496a3
Aditya Raj <adityarajofficial0096@gmail.com>
2024-05-31 12:10:06 +0530

    function added in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,12 @@ var Paths = {
     SCSS_TOOLKIT_SOURCES: './assets/scss/argon-dashboard.scss',
     SCSS: './assets/scss/**/**'
   };
+
+  gulp.task('compile-scss', function() {
+    return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write(Paths.HERE))
+      .pipe(gulp.dest(Paths.CSS));
+  });
