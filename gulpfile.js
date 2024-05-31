ea0c3c139c3388787e4fc0f8df13e645539108a0
Aditya Raj <adityarajofficial0096@gmail.com>
2024-05-31 12:09:17 +0530

    Variable Paths added in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,11 @@ var sass = require('gulp-sass')(require('sass'));
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
+
+var Paths = {
+    HERE: './',
+    DIST: 'dist/',
+    CSS: './assets/css/',
+    SCSS_TOOLKIT_SOURCES: './assets/scss/argon-dashboard.scss',
+    SCSS: './assets/scss/**/**'
+  };
